06665ca946572d9ab68fa374f805087c0ef3d2fa
nashare <tatiana.lavr@gmail.com>
2023-10-26 08:35:38 -0700

    learn button

--- components/header/headerLinks/loggedInTrueHeaderLinks.js
+++ components/header/headerLinks/loggedInTrueHeaderLinks.js
@@ -1,5 +1,7 @@
 export const loggedInTrueHeaderLinks = 
-    `
+    `   <a href='/categories/categories.html' class='link header-link'>
+            <li>Learn</li>
+        </a>
         <a href='/' class='link header-link' id='log-out'>
             <li>Log Out</li>
         </a>
